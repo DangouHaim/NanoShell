a5f2973c3ea5281b4467ac114e3a2d7f319eb568
DangouHaim <dangouhaim@gmail.com>
2026-06-25 08:55:25 +0200

    refactor: rename NanoShell to NanoShell.UI with service-based code-behind

---
+++ NanoShell.UI/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace NanoShell.UI;
+
+public partial class App : Application
+{
+}
---
+++ NanoShell.UI/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Threading;
+using NanoShell.Interop;
+using NanoShell.Services;
+
+namespace NanoShell.UI;
+
+public partial class MainWindow : Window
+{
+    private readonly KeyboardService _keyboardService;
+    private readonly AppBarService _appBarService;
+    private readonly WindowStateService _windowStateService;
+    private readonly WindowAutoManagerService _windowAutoManager;
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+
+        IntPtr hwnd = new WindowInteropHelper(this).Handle;
+        int extendedStyle = NativeMethods.GetWindowLong(hwnd, Constants.GWL_EXSTYLE);
+        NativeMethods.SetWindowLong(hwnd, Constants.GWL_EXSTYLE, extendedStyle | Constants.WS_EX_NOACTIVATE | Constants.WS_EX_TOOLWINDOW);
+
+        _windowAutoManager.Start();
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _keyboardService = new KeyboardService();
+        _appBarService = new AppBarService();
+        _windowStateService = new WindowStateService();
+        _windowAutoManager = new WindowAutoManagerService(Dispatcher, _windowStateService);
+
+        Top = SystemParameters.PrimaryScreenHeight - 50;
+        Width = SystemParameters.PrimaryScreenWidth;
+        _appBarService.RegisterAppBar(Height);
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        _appBarService.RegisterAppBar(Height);
+    }
+
+    private void Window_Closed(object sender, EventArgs e)
+    {
+        _windowAutoManager?.Dispose();
+        _appBarService.RegisterAppBar();
+    }
+
+    private DateTime _lastBackTapTime = DateTime.MinValue;
+    private DispatcherTimer _backTapTimer;
+
+    private void HandleTapWithDoubleTap(ref DateTime lastTapTime, ref DispatcherTimer timer, Action singleTap, Action doubleTap)
+    {
+        timer?.Stop();
+        var now = DateTime.Now;
+        if ((now - lastTapTime).TotalMilliseconds < 300)
+        {
+            doubleTap();
+            lastTapTime = DateTime.MinValue;
+        }
+        else
+        {
+            lastTapTime = now;
+            DispatcherTimer localTimer = null;
+            localTimer = new DispatcherTimer(TimeSpan.FromMilliseconds(300), DispatcherPriority.Normal,
+                (s, args) =>
+                {
+                    singleTap();
+                    localTimer?.Stop();
+                },
+                Dispatcher.CurrentDispatcher);
+            timer = localTimer;
+            localTimer.Start();
+        }
+    }
+
+    private void CancelPendingTap(ref DateTime lastTapTime, ref DispatcherTimer timer)
+    {
+        timer?.Stop();
+        lastTapTime = DateTime.MinValue;
+    }
+
+    private void BtnBack_Gesture(object sender, StylusSystemGestureEventArgs e)
+    {
+        switch (e.SystemGesture)
+        {
+            case SystemGesture.Tap:
+                HandleTapWithDoubleTap(ref _lastBackTapTime, ref _backTapTimer,
+                    () => _keyboardService.SimulateKeyCombination(Key.LeftAlt, Key.Left),
+                    () => _keyboardService.SimulateKeyPress(Key.Escape));
+                break;
+            case SystemGesture.RightTap:
+                CancelPendingTap(ref _lastBackTapTime, ref _backTapTimer);
+                _keyboardService.SimulateKeyCombination(Key.LeftAlt, Key.F4);
+                break;
+        }
+        e.Handled = true;
+    }
+
+    private void BtnCloseAll_Gesture(object sender, StylusSystemGestureEventArgs e)
+    {
+        switch (e.SystemGesture)
+        {
+            case SystemGesture.Tap:
+                _keyboardService.SimulateKeyCombination(Key.LWin, Key.D);
+                break;
+            case SystemGesture.RightTap:
+                _appBarService.OpenTouchKeyboard();
+                break;
+        }
+        e.Handled = true;
+    }
+
+    private DateTime _lastTaskViewTapTime = DateTime.MinValue;
+    private DispatcherTimer _taskViewTapTimer;
+
+    private void BtnTaskView_Gesture(object sender, StylusSystemGestureEventArgs e)
+    {
+        switch (e.SystemGesture)
+        {
+            case SystemGesture.Tap:
+                HandleTapWithDoubleTap(ref _lastTaskViewTapTime, ref _taskViewTapTimer,
+                    () => _keyboardService.SimulateKeyCombination(Key.LWin, Key.Tab),
+                    () => _keyboardService.SimulateKeyCombination(Key.LeftAlt, Key.Tab));
+                break;
+            case SystemGesture.RightTap:
+                CancelPendingTap(ref _lastTaskViewTapTime, ref _taskViewTapTimer);
+                _appBarService.RegisterAppBar(Height);
+                _windowStateService.ToggleMaximize();
+                break;
+        }
+        e.Handled = true;
+    }
+
+    private void Panel_Gesture(object sender, StylusSystemGestureEventArgs e)
+    {
+        if (e.SystemGesture == SystemGesture.RightTap)
+        {
+            _keyboardService.SimulateKeyPress(Key.PrintScreen);
+            e.Handled = true;
+        }
+    }
+}
