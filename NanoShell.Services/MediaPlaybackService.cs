761415943a27996e90354b6bfbe9281196e7d92e
DangouHaim <dangouhaim@gmail.com>
2026-07-06 12:10:09 +0200

    feat: add MediaPlaybackService wrapping WinRT SMTC

---
+++ NanoShell.Services/MediaPlaybackService.cs
@@ -0,0 +1,49 @@
+using Windows.Media;
+using Windows.Media.Control;
+
+namespace NanoShell.Services;
+
+public sealed class MediaPlaybackService
+{
+    private GlobalSystemMediaTransportControlsSessionManager? _manager;
+
+    public MediaPlaybackService()
+    {
+        try
+        {
+            var task = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().AsTask();
+            task.Wait(5000);
+            if (task.IsCompletedSuccessfully)
+                _manager = task.Result;
+        }
+        catch
+        {
+            _manager = null;
+        }
+    }
+
+    public bool IsVideoPlaying()
+    {
+        if (_manager is null) return false;
+
+        try
+        {
+            var sessions = _manager.GetSessions();
+            foreach (var session in sessions)
+            {
+                var info = session.GetPlaybackInfo();
+                if (info is null) continue;
+                if (info.PlaybackStatus == GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing &&
+                    info.PlaybackType == MediaPlaybackType.Video)
+                {
+                    return true;
+                }
+            }
+        }
+        catch
+        {
+        }
+
+        return false;
+    }
+}
